64b4edbbcdc1a9b3fbd225c50083e6c7a058a103
hehe <duy.nv@globalflow.com.vn>
2018-07-11 17:22:19 +0700

    Param type format

--- src/FormatNumber.php
+++ src/FormatNumber.php
@@ -8,32 +8,57 @@ class FormatNumber {
 	 * @return string
 	 * Use to convert large positive numbers in to short form like 1K+, 100K+, 199K+, 1M+, 10M+, 1B+ etc
 	 */
-	function format($n) {
-		if ($n > 0 && $n < 1000) {
-			// 1 - 999
-			$n_format = floor($n);
-			$suffix = '';
-		} else if ($n >= 1000 && $n < 1000000) {
+	function format($n, $code) {
+		// if ($n > 0 && $n < 1000) {
+		// 	// 1 - 999
+		// 	$n_format = floor($n);
+		// 	$suffix = '';
+		// } else if ($n >= 1000 && $n < 1000000) {
+		// 	// 1k-999k
+		// 	$n_format = floor($n / 1000);
+		// 	$suffix = 'K+';
+		// } else if ($n >= 1000000 && $n < 1000000000) {
+		// 	// 1m-999m
+		// 	$n_format = floor($n / 1000000);
+		// 	$suffix = 'M+';
+		// } else if ($n >= 1000000000 && $n < 1000000000000) {
+		// 	// 1b-999b
+		// 	$n_format = floor($n / 1000000000);
+		// 	$suffix = 'B+';
+		// } else if ($n >= 1000000000000) {
+		// 	// 1t+
+		// 	$n_format = floor($n / 1000000000000);
+		// 	$suffix = 'T+';
+		// } else {
+		// 	$n_format = 0;
+		// 	$suffix = '';
+		// }
+		switch ($code) {
+		case 'K':
 			// 1k-999k
 			$n_format = floor($n / 1000);
-			$suffix = 'K+';
-		} else if ($n >= 1000000 && $n < 1000000000) {
+			$suffix = 'K';
+			break;
+		case 'M':
 			// 1m-999m
 			$n_format = floor($n / 1000000);
-			$suffix = 'M+';
-		} else if ($n >= 1000000000 && $n < 1000000000000) {
+			$suffix = 'M';
+			break;
+		case 'B':
 			// 1b-999b
 			$n_format = floor($n / 1000000000);
-			$suffix = 'B+';
-		} else if ($n >= 1000000000000) {
+			$suffix = 'B';
+			break;
+		case 'T':
 			// 1t+
 			$n_format = floor($n / 1000000000000);
-			$suffix = 'T+';
-		} else {
-			$n_format = 0;
-			$suffix = '';
-		}
+			$suffix = 'T';
+			break;
 
-		return !empty($n_format . $suffix) ? $n_format . $suffix : 0;
+		default:
+			# code...
+			break;
+		}
+		return $n_format;
 	}
 }
